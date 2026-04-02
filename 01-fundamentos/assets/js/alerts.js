aad76355e1a1bd1b8679b79a562fe0ec44346a68
Andrés Muñoz <andres2078@gmail.com>
2026-04-01 20:04:21 -0500

    Add JavaScript basic concepts: variables, comments, prompt, confirm,alert

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,13 @@
+
+
+alert('Hola Mundo');
+
+
+let nombre = prompt('¿Cuál es tu nombre?');
+console.log(nombre);
+console.log('*****' + nombre + '*****'); // ''
+
+const selection = confirm('¿Esta seguro de borrar esto?');
+console.log(selection);
+
+// console.log(global);
