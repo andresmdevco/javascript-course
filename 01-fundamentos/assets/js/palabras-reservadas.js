a276e7483f63c253f62cd6bc775366f29c8fc06c
Andrés Muñoz <andres2078@gmail.com>
2026-04-02 17:40:19 -0500

    add reserved words examples

---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,25 @@
+
+
+let constante = 123;
+
+//const
+
+// if
+
+// try 
+
+// while 
+// for()
+
+// enum 
+
+let _objeto$123 = 123;
+let precio99_99 = 123;
+
+// Lower Camel Case
+let jugadorConPuntajeMasAlto = 'Fernando';
+
+// Upper Camel Case
+class JuegoAnio {
+
+}
