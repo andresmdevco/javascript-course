eb7fc5297fc533ad9dfde8055773c6113aac8649
Andrés Muñoz <andres2078@gmail.com>
2026-04-04 20:19:11 -0500

    Add function return examples

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -1,7 +1,11 @@
 
 function saludar(nombre) {
-    console.log( arguments );
-    console.log('Hola ' + nombre);
+    // console.log( arguments );
+    // console.log('Hola ' + nombre);
+    return [1, 2];
+
+    // Esto nunca se va a ejecutar
+    console.log('Soy un código que esta después del return');
 }
 
 // Conocida como una función anónima
@@ -20,11 +24,36 @@ const saludarFlecha2 = (nombre) => {
 }
 
 
-saludar('Andrés', 25, false, 'Colombia');
+const retornoDeSaludar = saludar('Andrés', 25, false, 'Colombia'); // 1 
+// console.log(retornoDeSaludar[0], retornoDeSaludar[1]);
+
 // saludar2('Andrés');
  
-saludarFlecha();
-saludarFlecha2('Avril');
+// saludarFlecha();
+// saludarFlecha2('Avril');
+
+function sumar(a, b) {
+    return a + b;
+}
+
+// const sumar2 = (a, b) => {
+//     return a + b;
+// }
+
+const sumar2 = (a, b) => a + b;
+
+
+function getAleatorio() {
+    return Math.random();
+}
+
+// Transformar la anterior función en una función de flecha que no tenga llaves { }
+
+const getAleatorio2 = () => Math.random();
+
+console.log(sumar2(2, 7));
+console.log(getAleatorio2());
+
 
 
 
