88e6ff3b3994a7c041366741071aac8e124fc6d5
Andrés Muñoz <andres2078@gmail.com>
2026-04-04 22:06:48 -0500

    Add examples of implicit return and destructuring

---
+++ 01-fundamentos/assets/js/protip-retorno.js
@@ -0,0 +1,61 @@
+
+// function crearPersona(nombre, apellido) {
+//     return {nombre, apellido}
+// }
+
+const crearPersona = (nombre, apellido) => ({nombre, apellido});
+
+
+const persona = crearPersona('Andrés', 'Muñoz');
+console.log(persona);
+
+
+function imprimeArgumentos() {
+    console.log(arguments);
+}
+
+const imprimeArgumentos2 = (edad, ...args) => {
+    // console.log({edad, args});
+    return args;
+}
+
+const [vivo, casado, nombre, saludo] = imprimeArgumentos2(10, true, false, 'Andrés', 'Hola');
+
+imprimeArgumentos(10, true, false, 'Andrés', 'Hola');
+console.log({vivo, casado, nombre, saludo})
+
+
+
+const {apellido: nuevoApellido} = crearPersona('Andrés', 'Muñoz');
+console.log({nuevoApellido});
+
+
+// Desestructuración de argumentos
+const tony = {
+    nombre: 'Tony Stark',
+    codeName: 'Ironman',
+    vivo: false,
+    edad: 40,
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster']
+};
+
+
+// const imprimePropiedades = (personaje) => {
+
+//     console.log('nombre', personaje.nombre);
+//     console.log('codeName', personaje.codeName);
+//     console.log('vivo', personaje.vivo);
+//     console.log('edad', personaje.edad);
+//     console.log('trajes', personaje.trajes);
+// }
+
+const imprimePropiedades = ({nombre, codeName, vivo, edad = 15, trajes}) => {
+
+    console.log({nombre});
+    console.log({codeName});
+    console.log({vivo});
+    console.log({edad});
+    console.log({trajes});
+}
+
+imprimePropiedades(tony);
