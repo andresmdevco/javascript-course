307419c520ade6577eaa5d09e06f761c42f6831a
Andrés Muñoz <andres2078@gmail.com>
2026-04-06 09:44:14 -0500

    Add alternatives to if else using objects and arrays

--- 01-fundamentos/assets/js/if-else.js
+++ 01-fundamentos/assets/js/if-else.js
@@ -32,3 +32,36 @@ if (dia === 0) {
 } else {
     console.log('No es lunes, martes o domingo...');
 }
+
+// Sin usar If Else, o Switch, únicamente objetos o arreglos
+dia = 5;
+
+const diasLetras = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miércoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sábado',
+}
+
+// dia de la semana
+console.log(diasLetras[dia] || 'Día no definido');
+
+
+const diasLetras2 = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+console.log(diasLetras2[dia] || 'Día no definido');
+
+
+const diasLetras3 = {
+    0: ()=> 'Domingo - 0',
+    1: ()=> 'Lunes - 1',
+    2: ()=> 'Martes - 2',
+    3: ()=> 'Miércoles - 3',
+    4: ()=> 'Jueves - 4',
+    5: ()=> 'Viernes - 5',
+    6: ()=> 'Sábado - 6',
+}
+
+console.log(diasLetras3[dia]());
