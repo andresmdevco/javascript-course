5f6c3a39b9eb7d318c472b19e8221e4d771b1062
Andrés Muñoz <andres2078@gmail.com>
2026-04-10 11:35:16 -0500

    feat: add computer turn logic and stop button event

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -15,20 +15,24 @@ let puntosJugador = 0,
 
 // Referencias del HTML
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+
 const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
+
 const puntosHTML = document.querySelectorAll('small');
 
 // Esta función crea un nuevo deck (Baraja)
 const crearDeck = () => {
     
-    for(let i = 2; i <= 10; i++) {
-        for(let tipo of tipos){
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos){
             deck.push(i + tipo);
         }
     }
 
-    for(let tipo of tipos){
-        for(let especial of especiales){
+    for (let tipo of tipos) {
+        for (let especial of especiales) {
             deck.push(especial + tipo);
         }
     }
@@ -66,6 +70,28 @@ const valorCarta = (carta) => {
 }
 
 
+// turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+    
+    do {
+
+    const carta = pedirCarta();
+
+    puntosComputadora = puntosComputadora + valorCarta(carta);
+    puntosHTML[1].innerText = puntosComputadora;
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;  // 3H, JD
+    imgCarta.classList.add('carta')
+    divCartasComputadora.append(imgCarta);
+
+    if(puntosMinimos > 21) {
+        break;
+    }
+
+    } while((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+}
+
 
 // Eventos
 btnPedir.addEventListener('click', () => {
@@ -81,11 +107,25 @@ btnPedir.addEventListener('click', () => {
     imgCarta.classList.add('carta')
     divCartasJugador.append(imgCarta);
 
-    if(puntosJugador > 21) {
+    if (puntosJugador > 21) {
         console.warn('Has perdido');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     } else if (puntosJugador === 21) {
         console.warn('21, Has Ganado!');
         btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador);
     }
 });
+
+btnDetener.addEventListener('click', () => {
+
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+
+    turnoComputadora(puntosJugador);
+});
+
+
