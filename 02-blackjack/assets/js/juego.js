e6d8520a5ab118305f350f65e15bea1b184a5e9f
Andrés Muñoz <andres2078@gmail.com>
2026-04-09 23:24:24 -0500

    feat: add draw card button event and player score update

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -10,6 +10,12 @@ let deck = [];
 const tipos = ['C','D','H','S'];
 const especiales = ['A','J','Q','K'];
 
+let puntosJugador = 0,
+    puntosComputadora = 0;
+
+// Referencias del HTML
+const btnPedir = document.querySelector('#btnPedir')
+const puntosHTML = document.querySelectorAll('small');
 
 // Esta función crea un nuevo deck (Baraja)
 const crearDeck = () => {
@@ -42,11 +48,7 @@ const pedirCarta = () => {
     if (deck.length === 0) {
         throw 'No hay cartas en el deck';
     }
-
     const carta = deck.pop();
-
-    console.log(deck);
-    console.log(carta); // la carta debe ser de la baraja 
     return carta;
 }
 
@@ -63,5 +65,14 @@ const valorCarta = (carta) => {
 }
 
 
-const valor = valorCarta(pedirCarta())
-console.log({valor})
+
+// Eventos
+btnPedir.addEventListener('click', () => {
+
+    const carta = pedirCarta();
+
+    puntosJugador = puntosJugador + valorCarta(carta);
+    puntosHTML[0].innerText = puntosJugador;
+
+    
+});
