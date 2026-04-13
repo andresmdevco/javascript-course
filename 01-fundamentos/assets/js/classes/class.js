ffeb8ca44b53aa7b040f26c19176a916149912ca
Andrés Muñoz <andres2078@gmail.com>
2026-04-13 17:17:26 -0500

    feat: add static properties, getters and methods to class

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,6 +1,17 @@
 
 
 class Persona {
+    
+    static _conteo = 0; // conteo de las instacias de la clase que se han inicializado
+    static get conteo() {
+        return Persona._conteo + ' instancias'
+    }
+    // método estático
+    static mensaje() { 
+        console.log(this.nombre); // undefined
+        console.log('Hola a todos, soy un método estático');
+    }
+
     nombre = '';
     codigo = '';
     frase = '';
@@ -11,6 +22,8 @@ class Persona {
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+
+        Persona._conteo++;
     }
 
     // sets y gets
@@ -35,16 +48,28 @@ class Persona {
 
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Un gran poder conlleva una gran responsabilidad');
 const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman');
+
 // console.log(ironman);
 
 // spiderman.quienSoy();
 // ironman.quienSoy();
+
 spiderman.miFrase();
 // ironman.miFrase();
 
 spiderman.setComidaFavorita = 'El pay de cereza de la tía May';
 // spiderman.comida = 'Duende Verde';
 
-console.log(spiderman.getComidaFavorita);
+// console.log(spiderman.getComidaFavorita);
 
-console.log(spiderman);
+// console.log(spiderman);
+// Persona._conteo = 2;
+console.log('Conteo estático', Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
+
+// Definir propiedades estáticas fuera de la clase
+Persona.propiedadExterna = 'Hola Mundo';
+
+console.log(Persona.propiedadExterna);
+console.log(Persona);
